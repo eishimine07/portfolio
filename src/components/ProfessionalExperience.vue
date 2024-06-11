ae091de29fba91408b9f4a9e6a0769d093306751
Erick Ishimine <erick.ishimine@gmail.com>
2024-06-11 00:07:12 -0300

    add image and links

--- src/components/ProfessionalExperience.vue
+++ src/components/ProfessionalExperience.vue
@@ -1,41 +1,46 @@
 <script setup lang="ts">
+import { useDisplay } from 'vuetify'
 import PortfolioSection from './PortfolioSection.vue'
+import { computed } from 'vue';
 
 type Experience = {
   title: string
   period: string
   description: string
   image: string
+  url: string
 }
 
-const professionalExperience: Experience[] = [
-  { title: 'Engenheiro de Software - Infineat', period: 'Janeiro/2024 - Presente', description: 'Participei da análise de requisitos, definição de escopo e estimativa de tempo de desenvolvimento dos projetos.' },
-  { title: 'Desenvolvedor Freelancer - Infineat', period: 'Setembro/2022 - Janeiro/2024', description: 'Planejei e desenvolvi funcionalidades tanto no frontend quanto no backend da aplicação web.' },
-  { title: 'Líder Técnico - Delivery Direto', period: 'Janeiro/2023 - Dezembro/2023', description: 'Participei da análise de requisitos, definição de escopo e estimativa de tempo de desenvolvimento dos projetos.' },
-  { title: 'Desenvolvedor Full Stack - Delivery Direto', period: 'Agosto/2019 - Janeiro/2023' , description: 'Trabalhei no desenvolvimento de diversas funcionalidades no produto. Algumas delas foram: Programa de fidelidade; Agendamento de campanhas de e-mail e push notification; Disponibilização do Pix como meio de pagamento.' },
-  { title: 'Estagiário de Desenvolvimento Web - Delivery Direto', period: 'Agosto/2018 - Agosto/2019' , description: 'Contribuí para o desenvolvimento de novas funcionalidades para os clientes finais e de uso interno.' },
-  { title: 'Mentor - PoliRetribua', period: 'Março/202s2 - Maio/2024', description: 'Procuro ouvir e  tentar compreender as principais dificuldades dos alunos ingressantes nos cursos de engenharia na Escola Politécnica da USP.' },
-]
-const experiences = [
+const experiences: Experience[] = [
   {
     title: 'Engenheiro de Software - Infineat',
     period: 'Janeiro/2024 - Presente',
     description: 'Participei da análise de requisitos, definição de escopo e estimativa de tempo de desenvolvimento dos projetos.',
-    image: 'https://fastly.picsum.photos/id/730/640/480.jpg?hmac=ap_Buv8mFqdRq11fWmSq2qgO4Ph7cDNLaALIkvrG9TI',
+    image: '/infineat.png',
+    url: 'https://www.infineat.com.br/',
   },
   {
     title: 'Líder Técnico - Delivery Direto',
     period: 'Janeiro/2023 - Dezembro/2023',
-    description: 'Participei da análise de requisitos, definição de escopo e estimativa de tempo de desenvolvimento dos projetos.',
-    image: 'https://fastly.picsum.photos/id/730/640/480.jpg?hmac=ap_Buv8mFqdRq11fWmSq2qgO4Ph7cDNLaALIkvrG9TI',
+    description: 'Desenvolvimento de uma plataforma de entrega white label, abrangendo desde a aplicação/web até o website para gerenciamento do negócio de restaurantes. Tenho participado de vários projetos, incluindo: Manutenção e melhoria em sistema legado construído com CakePHP e JQuery; Implementação e integração de novas funcionalidades construídas em Flutter no sistema legado, através de WebView; Manutenção e desenvolvimento de um novo sistema de gerenciamento construído com Nuxt.js para substituir o sistema legado; Integração com pagamentos online via Stripe e Vindi.',
+    image: '/deliverydireto.png',
+    url: 'https://site.deliverydireto.com.br/',
   },
   {
     title: 'Mentor - PoliRetribua',
     period: 'Março/202s2 - Maio/2024',
-    description: 'Procuro ouvir e  tentar compreender as principais dificuldades dos alunos ingressantes nos cursos de engenharia na Escola Politécnica da USP.',
-    image: 'https://fastly.picsum.photos/id/730/640/480.jpg?hmac=ap_Buv8mFqdRq11fWmSq2qgO4Ph7cDNLaALIkvrG9TI',
+    description: `O Projeto PoliRetribua é formado por um grupo de profissionais voluntários dedicados a apoiar e fortalecer estudantes em condições de vulnerabilidade socioeconômica da Escola Politécnica da USP. O objetivo é garantir a permanência desses estudantes na universidade até a conclusão da graduação, por meio de mentorias e bolsas de estudo, ajudando a evitar a evasão escolar.\nComo mentor voluntário, tive a oportunidade de orientar alguns alunos a enfrentar os desafios iniciais do curso, dando dicas sobre como organizar a rotina de estudos e encontrar o método de aprendizado que mais funciona para cada um. Além disso, procurava mostar a importância de aproveitar ao máximo o ambiente universitário, participando de grupos de extensão, conhecendo outros institutos e praticando esportes.`,
+    image: '/poliretribua.png',
+    url: 'https://poliretribua.org.br/',
   },
 ]
+
+const { mdAndUp } = useDisplay()
+
+const imgSize = computed(() => ({
+  height: mdAndUp ? 120 : 90,
+  width: mdAndUp ? 160 : 120,
+}))
 </script>
 
 <template>
@@ -67,13 +72,19 @@ const experiences = [
               {{ exp.description }}
             </v-card-text>
           </div>
-
-          <v-img
-            class="ma-3 rounded"
-            height="120"
-            :src="exp.image"
-            width="160"
-          />
+          
+          <div class="ma-3 rounded bg-white pa-4 ma-auto">
+            <a
+              :href="exp.url"
+              target="_blank"
+            >
+              <v-img
+                height="120"
+                :src="exp.image"
+                width="160"
+              />
+            </a>
+          </div>
         </div>
       </v-card>
     </v-hover>
